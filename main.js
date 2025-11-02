0b18d68907c04cc0c49ee0c44371f817e66588cf
geooooo <geo97it@gmail.com>
2025-11-02 08:53:47 +0300

    update

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+// Придерживаться максимально старой редакции EcmaScript
+
 var imgPreviewElement = null;
 var shadowElement = null;
 var imageBaseUrl = "assets/"
@@ -11,6 +13,104 @@ var imagesToPreload = [
     "certs/stepik.png",
     "certs/coursera.png",
 ];
+
+var skills = {
+    "Языки": [
+        new Skill("C", 3),
+        new Skill("Go", 2),
+        new Skill("Rust", 1),
+        new Skill("SQL", 2),
+        new Skill("Python", 3),
+        new Skill("Dart", 3),
+        new Skill("JavaScript", 3),
+        new Skill("TypeScript", 3),
+        new Skill("Node", 3),
+        new Skill("HTML", 3),
+        new Skill("CSS", 3),
+        new Skill("PHP", 2),
+        new Skill("C++", 1),
+        new Skill("Assembler x86", 2),
+    ],
+    "Фреймворки, библиотеки, SDK и т.п.": {
+        "Go": [
+            new Skill("Gin", 1),
+            new Skill("Gorilla", 1),
+            new Skill("Gorm", 1),
+            new Skill("Sqlx", 1),
+        ],
+        "Dart/Flutter": [
+            new Skill("AngularDart", 3),
+            new Skill("RxDart", 2),
+            new Skill("Redux", 3),
+            new Skill("Bloc", 2),
+            new Skill("Cubit", 2),
+            new Skill("Provider", 3),
+            new Skill("GetX", 3),
+        ],
+        "JavaScript/TypeScript/Node": [
+            new Skill("SCSS", 3),
+            new Skill("Less", 3),
+            new Skill("Webpack", 3),
+            new Skill("React", 2),
+            new Skill("Angular 2+", 2),
+            new Skill("Vue", 2),
+            new Skill("Electron", 3),
+            new Skill("Microfrontends", 2),
+            new Skill("Web Components", 3),
+            new Skill("Rx", 2),
+            new Skill("Express.js", 3),
+            new Skill("Nest.js", 1),
+        ],
+        "Python": [
+            new Skill("Flask", 3),
+            new Skill("Bottle", 3),
+        ],
+    },
+    "Базовые инструменты": [
+        new Skill("SOLID", 3),
+        new Skill("Git", 3),
+        new Skill("Gitlab", 2),
+        new Skill("CI/CD", 1),
+        new Skill("Linux", 3),
+        new Skill("Bash", 3),
+        new Skill("REST API", 3),
+        new Skill("GraphQL", 2),
+        new Skill("RPC", 2),
+        new Skill("GRPC", 2),
+        new Skill("SQLite", 3),
+        new Skill("MySQL", 2),
+        new Skill("PostgreSQL", 1),
+        new Skill("MongoDB", 1),
+        new Skill("Redis", 1),
+        new Skill("Memcached", 1),
+        new Skill("Firebase", 1),
+    ],
+    "Soft-скилы": [
+        new Skill("English A2"),
+        new Skill("Agile"),
+        new Skill("Kanban"),
+        new Skill("Scrum"),
+        new Skill("Управление проектами"),
+        new Skill("Teamlead"),
+    ],
+    "Приложения": [
+        new Skill("Postman"),
+        new Skill("VSCode"),
+        new Skill("Android Studio"),
+        new Skill("AI tools for dev"),
+        new Skill("Trello"),
+        new Skill("Wrike"),
+        new Skill("Asana"),
+        new Skill("Jira"),
+        new Skill("Photoshop"),
+        new Skill("Figma"),
+    ],
+};
+
+function Skill(name, level) {
+    this.name = name;
+    this.level = level;
+}
 
 function main() {
     preloadImages();
@@ -33,10 +133,62 @@ function preloadImages() {
 function onWindowLoad() {
     shadowElement = window.document.querySelector(".shadow");
     var coursesElement = window.document.querySelector(".courses");
-    
+    var skillsContentElement = window.document.querySelector(".section-skills .section-content");
+    var achievementsLinkElement = window.document.querySelector("#achievementsLink");
+
     window.addEventListener("beforeprint", onBeforePrint);
     window.document.body.addEventListener("click", onClickOutside);
     coursesElement.addEventListener("click", onCoursesClick);
+    achievementsLinkElement.addEventListener("click", onAchievementsLinkClick);
+
+    renderSkills(skillsContentElement);
+}
+
+function renderSkills(containerElement) {
+    Object.keys(skills).forEach(function(mainCategoryName) {
+        var h3Element = window.document.createElement("h3");
+        h3Element.className = "skills-type";
+        h3Element.textContent = mainCategoryName;
+        containerElement.appendChild(h3Element);
+
+        var listElement = window.document.createElement("ul");
+        listElement.className = "skills";
+        containerElement.appendChild(listElement);
+        
+        var mainCategorySkills = skills[mainCategoryName];
+        Object.keys(mainCategorySkills).forEach(function(subCategoryName, i) {
+            var skill = mainCategorySkills[i];
+            var listItemElement = window.document.createElement("li");
+
+            if (skill instanceof Skill) {
+                listItemElement.className = "skills-item";
+                var innerHTML = '<div class="skill-name">{0}</div>'.replace("{0}", skill.name);
+                if (skill.level != null) {
+                    innerHTML += '<div class="skill-level" data-level="{1}"><b></b><b></b><b></b></div>'.replace("{1}", skill.level);
+                }
+                listItemElement.innerHTML = innerHTML;
+                listElement.appendChild(listItemElement);
+            } else {
+                listItemElement.innerHTML = '<h4 class="skills-type">' + subCategoryName + '</h4>';
+                listElement.appendChild(listItemElement);
+                
+                var subListElement = window.document.createElement("ul");
+                subListElement.className = "subskills";
+                listItemElement.appendChild(subListElement);
+
+                Object.keys(mainCategorySkills[subCategoryName]).forEach(function(_, i) {
+                    var skill = mainCategorySkills[subCategoryName][i];
+                    
+                    var subListItemElement = window.document.createElement("li");
+                    subListItemElement.className = "skills-item";
+                    subListItemElement.innerHTML = 
+                        '<div class="skill-name">{0}</div>'.replace("{0}", skill.name) + 
+                        '<div class="skill-level" data-level="{1}"><b></b><b></b><b></b></div>'.replace("{1}", skill.level);
+                    subListElement.appendChild(subListItemElement);
+                })
+            }
+        });
+    });
 }
 
 function onBeforePrint() {
@@ -60,6 +212,11 @@ function onCoursesClick(event) {
             onToggleCoursesList(headerElement);
         }
     }
+}
+
+function onAchievementsLinkClick() {
+    var coursesHeaderElements = window.document.querySelectorAll(".courses-header");
+    coursesHeaderElements.forEach(onToggleCoursesList);
 }
 
 function onImageClickForPreview(imgElement) {
